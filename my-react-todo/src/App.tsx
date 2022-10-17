5b86f5e762f4bad8b4fc1dfabc940fe2e84897e4
Cleilson Sampaio <jcleilsonsv@hotmail.com>
2022-10-17 20:10:03 -0300

    Add header and footer

--- my-react-todo/src/App.tsx
+++ my-react-todo/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+import styles from './App.module.css'
 
 function App() {
   return (
     <div>
-      <h1>Olá mundo com CI-CD</h1>
+      <Header/>
+     <main className={styles.main}>
+      <h1> Conteúdo</h1>
+     </main>
+      <Footer/>
     </div>
   );
 }
---
+++ my-react-todo/src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import styles from "./Footer.module.css"
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <p>
+          <span>My react To-Do</span> @2022
+        </p>
+      </footer>
+  )
+}
+
+export default Footer
